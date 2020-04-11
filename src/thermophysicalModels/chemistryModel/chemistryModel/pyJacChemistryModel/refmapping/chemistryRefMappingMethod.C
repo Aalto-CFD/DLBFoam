0df0d6188ac82ca0e6f5b0b1e92a14cbf0d8b046
peltonp1 <peltonp1@aalto.fi>
2020-04-11 18:29:17 +0300

    broke the mixture fraction computation to smaller functions

--- src/thermophysicalModels/chemistryModel/chemistryModel/pyJacChemistryModel/refmapping/chemistryRefMappingMethod.C
+++ src/thermophysicalModels/chemistryModel/chemistryModel/pyJacChemistryModel/refmapping/chemistryRefMappingMethod.C
@@ -1,56 +1,7 @@
-/*---------------------------------------------------------------------------*\
-  =========                 |
-  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2016-2018 OpenFOAM Foundation
-     \\/     M anipulation  |
--------------------------------------------------------------------------------
-License
-    This file is part of OpenFOAM.
-
-    OpenFOAM is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-    for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
-
-\*---------------------------------------------------------------------------*/
-
 #include "chemistryRefMappingMethod.H"
-// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
-
-namespace Foam
-{
-/*
-chemistryRefMappingMethod::chemistryRefMappingMethod
-(
-    const dictionary& dict
-)
-:
-    dict_(dict),
-    coeffsDict_(dict.subDict("refmapping")),
-    active_(coeffsDict_.lookupOrDefault<Switch>("active", false))
-{
 
 
-    if (this->active_)
-    {
-       // scalar test = readScalar(mixFracDict.lookup("test"));
-    }
-}
-*/
-
-
-// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
-
-//chemistryRefMappingMethod::~chemistryRefMappingMethod(){}
+namespace Foam{
 
 
 
@@ -58,4 +9,3 @@ chemistryRefMappingMethod::chemistryRefMappingMethod
 
 }//namespace Foam
 
-// ************************************************************************* //
