d8af09c53c1feddb2953a880ac7874d1810636fa
Tekgül Bulut <tekgulb1@t20204-lr010.org.aalto.fi>
2020-04-14 13:53:33 +0300

    Derived class implementation

--- src/thermophysicalModels/chemistryModel/chemistryModel/pyJacChemistryModel/refmapping/chemistryRefMappingMethod.C
+++ src/thermophysicalModels/chemistryModel/chemistryModel/pyJacChemistryModel/refmapping/chemistryRefMappingMethod.C
@@ -1,18 +1,2 @@
 #include "chemistryRefMappingMethod.H"
 
-
-namespace Foam{
-
-
-//- Construct from dict and a list of species
-chemistryRefMappingMethod::chemistryRefMappingMethod(const dictionary& dict, const wordList& species)
-:
-dict_(dict),
-coeffsDict_(dict.subDict("refmapping")),
-active_(coeffsDict_.lookupOrDefault<Switch>("active", false)),
-mixture_fraction_(coeffsDict_.subDict("mixtureFractionProperties"), species)
-{
-}
-
-}//namespace Foam
-
