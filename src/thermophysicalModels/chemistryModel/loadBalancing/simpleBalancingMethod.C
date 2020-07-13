6b439562a333914553d48cf153abbd985dd70d4c
peltonp1 <petteri.peltonen@aalto.fi>
2020-07-14 02:33:49 +0300

    tried to implement the problem time computation

--- src/thermophysicalModels/chemistryModel/loadBalancing/simpleBalancingMethod.C
+++ src/thermophysicalModels/chemistryModel/loadBalancing/simpleBalancingMethod.C
@@ -2,38 +2,61 @@
 
 namespace Foam {
 
-chemistryLoadBalancingMethod::sendRecvInfo 
-simpleBalancingMethod::convert(const node_ptr& my_node, const chemistryLoad& my_org_load, const DynamicList<chemistryProblem>& problems) const{
 
+
+
+std::vector<int>
+simpleBalancingMethod::compute_problem_count(const node_ptr&                      sender,
+                                             const DynamicList<chemistryProblem>& problems) const {
+
+    std::vector<double> send_times;
+    double              total = 0.0;
+    for (const auto& child : sender->children) {
+
+        double time_diff = child->value.value - child->org_value.value;
+        runtime_assert(time_diff > 0, "A receiver has more load before balancing.");
+        send_times.push_back(time_diff);
+        total += time_diff;
+    }
+
+    double remaining = sender->org_value.value - total;
+    send_times.insert(send_times.begin(), remaining);
+
+
+    return times_to_problem_counts(send_times, problems);
+
+
+}
+
+chemistryLoadBalancingMethod::sendRecvInfo
+simpleBalancingMethod::convert(const node_ptr&                      my_node,
+                               const DynamicList<chemistryProblem>& problems) const {
 
     sendRecvInfo info;
 
     info.destinations       = {Pstream::myProcNo()};
     info.sources            = {Pstream::myProcNo()};
     info.number_of_problems = {problems.size()};
-    //return info;
+    // return info;
 
-    //receiver
-    if (my_node->parent->value.rank != -1){
-        
+    // receiver
+    if (my_node->parent->value.rank != -1) {
+
         info.sources.push_back(my_node->parent->value.rank);
-        info.number_of_problems.push_back(1);
+        // info.number_of_problems.push_back(1);
         return info;
-    }
-    else {
+    } else {
 
-        
-        for (const auto& child : my_node->children){
+        for (const auto& child : my_node->children) {
             info.destinations.push_back(child->value.rank);
-            info.number_of_problems.push_back(1);
-            info.number_of_problems[0] -= 1;
+            // info.number_of_problems.push_back(1);
+            // info.number_of_problems[0] -= 1;
         }
-
-
+        info.number_of_problems = compute_problem_count(my_node, problems);
+        // auto temp = compute_problem_count(my_node, problems);
     }
 
     return info;
-
 }
 
 void simpleBalancingMethod::update_state(const DynamicList<chemistryProblem>& problems) {
@@ -48,27 +71,22 @@ void simpleBalancingMethod::update_state(const DynamicList<chemistryProblem>& pr
 
     runtime_assert(my_node != nullptr, "My node not found from the node tree");
 
-    auto info = convert(my_node, my_load, problems);
+    auto info = convert(my_node, problems);
 
     set_state(info);
-
 }
 
-
-node_ptr simpleBalancingMethod::build_tree(const DynamicList<chemistryLoad>& loads) const{
+node_ptr simpleBalancingMethod::build_tree(const DynamicList<chemistryLoad>& loads) const {
 
     double treshold = 1.0 * compute_mean_load(loads);
 
-
     auto [big, small] = loadTree::divide(loads, treshold);
 
-    
     std::reverse(big.begin(), big.end());
 
     auto root = loadTree::new_node(chemistryLoad(-1, -1));
 
-        
-    for (const auto& v : big){
+    for (const auto& v : big) {
         auto child = loadTree::new_node(v);
         loadTree::add_child(root, child);
     }
@@ -78,32 +96,30 @@ node_ptr simpleBalancingMethod::build_tree(const DynamicList<chemistryLoad>& loa
     }
 
     return root;
-
 }
-
-
 
 std::vector<int> simpleBalancingMethod::times_to_problem_counts(
     const std::vector<double>& times, const DynamicList<chemistryProblem>& problems) const {
 
     std::vector<int> counts;
-    /*    double sum = 0.0;
+    double sum = 0.0;
+    int count = 0;
 
-        for (const auto& time : times){
-
-            for (auto it = problems.rbegin(); it != problems.rend(); ++it)
-            {
-                if (sum > time) {
-                    sum = 0.0;
-                    counts.push_back(count);
-                    count = 0;
-                }
-                count++;
-                sum+=problems[it].cpuTime;
+    for (const auto& time : times){
+        
+        for (auto it = problems.begin(); it != problems.end(); ++it)
+        {
+            if (sum >= time) {
+                sum = 0.0;
+                counts.push_back(count);
+                count = 0;
             }
+            count++;
+            sum+=it->cpuTime;
         }
+    }
 
-    */
+    
     return counts;
 }
 
