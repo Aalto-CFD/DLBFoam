b4c69ba23c3fe374f7b40379145fcb3536a4ab10
Tekgül Bulut <tekgulb1@t20204-lr010.org.aalto.fi>
2021-02-15 12:41:26 +0200

    gitignore working now

--- tutorials/chemFoam/gri/mechanism/pyJac/out/jacobs/jacob_10.h
+++
@@ -1,8 +0,0 @@
-#ifndef JACOB_HEAD_10
-#define JACOB_HEAD_10
-
-#include "header.h"
-
-void eval_jacob_10 (const double, const double, const double, const double*, const double*, const double*, double*, double*);
-
-#endif
