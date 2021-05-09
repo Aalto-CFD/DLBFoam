67a75a801779aa3a4b2a9ce56505f29c67cb199d
Tekgül Bulut <tekgulb1@t20204-lr010.org.aalto.fi>
2021-05-09 15:22:56 +0300

    update headers

--- tests/validation/pyjacTests/PSRTest/PSRTest.C
+++ tests/validation/pyjacTests/PSRTest/PSRTest.C
@@ -1,23 +1,21 @@
 /*---------------------------------------------------------------------------*\
   =========                 |
-  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
-     \\/     M anipulation  |
+  \\      /  F ield         | DLBFoam: Dynamic Load Balancing 
+   \\    /   O peration     | for fast reactive simulations
+    \\  /    A nd           | 
+     \\/     M anipulation  | 2020, Aalto University, Finland
 -------------------------------------------------------------------------------
 License
-    This file is part of OpenFOAM.
-
+    This file is part of DLBFoam library, derived from OpenFOAM.
+    https://github.com/blttkgl/DLBFoam
     OpenFOAM is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
     OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
     for more details.
-
     You should have received a copy of the GNU General Public License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
