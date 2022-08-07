f3b93b12eda0c732cfee1c2ec004555ca095d944
Morev Ilya <ilya.morev@aalto.fi>
2022-08-07 16:17:47 +0300

    add dynamicCode templates and process them in scripts
    Error messages of a form:
    wmakeLnInclude error: base directory 
    /src/thermophysicalModels/chemistryModel/ does not exist
    wmakeLnInclude error: base directory /src/ODE_DLB/ does not exist
    might be printed during dynamic compilation due to implementation of 
    wmakeLnIncludeAll script.
    This is expected and does not affect the compilation. This message is 
    created when wmakeLnIncludeAll tries to create lnInclude folders in 
    DLBFoam directories, after not finding them due to a bug.
    
    The reason is that sed command in wmakeLnIncludeAll 
    (https://github.com/OpenFOAM/OpenFOAM-9/blob/master/wmake/wmakeLnIncludeAll#L125)) 
    does not process path containing hyphens. Only after change to e.g.
    's%-I\([.$(a-zA-Z0-9_\)-/]*\)lnInclude.*%\1%'
    it works without errors, if full path is used in basicChemistryModel 
    file.
    To reduce amount of error print, I used DLBFOAM_PATH variable in 
    basicChemistryModel, which produces aforementioned errors instead of 
    "unknown option" error and print of help message for wmakeLnIncludeAll.

---
+++ etc/codeTemplates/dynamicCode/basicChemistryModelTemplate.C
@@ -0,0 +1,255 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | DLBFoam: Dynamic Load Balancing
+   \\    /   O peration     | for fast reactive simulations
+    \\  /    A nd           |
+     \\/     M anipulation  | 2022, Aalto University, Finland
+-------------------------------------------------------------------------------
+License
+    This file is part of DLBFoam library, derived from OpenFOAM.
+
+    https://github.com/Aalto-CFD/DLBFoam
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "makeChemistrySolver.H"
+#include "${method}ChemistryModel.H"
+#include "${solver}.H"
+
+#include "typedefThermo.H"
+
+#include "${specie}.H"
+
+#include "thermo.H"
+
+// EoS
+#include "${equationOfState}.H"
+
+// Thermo
+#include "${thermo}Thermo.H"
+#include "${energy}.H"
+
+// Transport
+#include "${transport}Transport.H"
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    // dynamicCode:
+    // SHA1 = ${SHA1sum}
+    //
+    // Unique function name that can be checked if the correct library version
+    // has been loaded
+    void ${typeName}_${SHA1sum}(bool load)
+    {
+        if (load)
+        {
+            // code that can be explicitly executed after loading
+        }
+        else
+        {
+            // code that can be explicitly executed before unloading
+        }
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#define ThermoPhysics                                                          \
+    ${transport}Transport${energy}${thermo}Thermo${equationOfState}${specie}
+
+#define standardChemistryModelCppTest 0
+#define TDACChemistryModelCppTest 1
+#define loadBalancedChemistryModelCppTest 2
+#define loadBalanced_pyJacChemistryModelCppTest 3
+
+namespace Foam
+{
+    typedefThermo
+    (
+        ${transport}Transport,
+        ${energy},
+        ${thermo}Thermo,
+        ${equationOfState},
+        ${specie}
+    );
+
+    #if ${method}ChemistryModelCppTest == loadBalanced_pyJacChemistryModelCppTest
+    defineChemistrySolver(standardChemistryModel, ThermoPhysics);
+    makeChemistrySolver(${solver}, standardChemistryModel, ThermoPhysics);
+    defineChemistrySolver(loadBalancedChemistryModel, ThermoPhysics);
+    makeChemistrySolver(${solver}, loadBalancedChemistryModel, ThermoPhysics);
+    #endif
+
+    #if ${method}ChemistryModelCppTest == loadBalancedChemistryModelCppTest
+    defineChemistrySolver(standardChemistryModel, ThermoPhysics);
+    makeChemistrySolver(${solver}, standardChemistryModel, ThermoPhysics);
+    #endif
+
+    defineChemistrySolver(${method}ChemistryModel, ThermoPhysics);
+    makeChemistrySolver(${solver}, ${method}ChemistryModel, ThermoPhysics);
+}
+
+
+
+#if ${method}ChemistryModelCppTest == TDACChemistryModelCppTest
+
+#include "makeChemistryReductionMethod.H"
+#include "makeChemistryTabulationMethod.H"
+
+namespace Foam
+{
+    defineChemistrySolver(standardChemistryModel, ThermoPhysics);
+    defineChemistryReductionMethod(nullArg, ThermoPhysics);
+    defineChemistryTabulationMethod(nullArg, ThermoPhysics);
+}
+
+#include "noChemistryReduction.H"
+#include "DAC.H"
+#include "DRG.H"
+#include "DRGEP.H"
+#include "EFA.H"
+#include "PFA.H"
+
+namespace Foam
+{
+    makeChemistryReductionMethod(none, ThermoPhysics);
+    makeChemistryReductionMethod(DAC, ThermoPhysics);
+    makeChemistryReductionMethod(DRG, ThermoPhysics);
+    makeChemistryReductionMethod(DRGEP, ThermoPhysics);
+    makeChemistryReductionMethod(EFA, ThermoPhysics);
+    makeChemistryReductionMethod(PFA, ThermoPhysics);
+}
+
+#include "noChemistryTabulation.H"
+#include "ISAT.H"
+namespace Foam
+{
+    makeChemistryTabulationMethod(none, ThermoPhysics);
+    makeChemistryTabulationMethod(ISAT, ThermoPhysics);
+}
+
+#endif
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#include "makeReaction.H"
+
+#include "ArrheniusReactionRate.H"
+#include "LandauTellerReactionRate.H"
+#include "thirdBodyArrheniusReactionRate.H"
+
+#include "JanevReactionRate.H"
+#include "powerSeriesReactionRate.H"
+
+#include "ChemicallyActivatedReactionRate.H"
+#include "FallOffReactionRate.H"
+
+#include "LindemannFallOffFunction.H"
+#include "SRIFallOffFunction.H"
+#include "TroeFallOffFunction.H"
+
+#include "LangmuirHinshelwoodReactionRate.H"
+
+#include "MichaelisMentenReactionRate.H"
+
+namespace Foam
+{
+    defineReaction(nullArg, ThermoPhysics);
+
+    makeIRNReactions(ArrheniusReactionRate, ThermoPhysics);
+    makeIRNReactions(LandauTellerReactionRate, ThermoPhysics);
+    makeIRNReactions(thirdBodyArrheniusReactionRate, ThermoPhysics);
+    makeIRReactions(JanevReactionRate, ThermoPhysics);
+    makeIRReactions(powerSeriesReactionRate, ThermoPhysics);
+
+    makeIRRPressureDependentReactions
+    (
+        FallOffReactionRate,
+        ArrheniusReactionRate,
+        LindemannFallOffFunction,
+        ThermoPhysics
+    );
+
+    makeIRRPressureDependentReactions
+    (
+        FallOffReactionRate,
+        ArrheniusReactionRate,
+        TroeFallOffFunction,
+        ThermoPhysics
+    );
+
+    makeIRRPressureDependentReactions
+    (
+        FallOffReactionRate,
+        ArrheniusReactionRate,
+        SRIFallOffFunction,
+        ThermoPhysics
+    );
+
+    makeIRRPressureDependentReactions
+    (
+        ChemicallyActivatedReactionRate,
+        ArrheniusReactionRate,
+        LindemannFallOffFunction,
+        ThermoPhysics
+    );
+
+    makeIRRPressureDependentReactions
+    (
+        ChemicallyActivatedReactionRate,
+        ArrheniusReactionRate,
+        TroeFallOffFunction,
+        ThermoPhysics
+    );
+
+    makeIRRPressureDependentReactions
+    (
+        ChemicallyActivatedReactionRate,
+        ArrheniusReactionRate,
+        SRIFallOffFunction,
+        ThermoPhysics
+    );
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#include "LangmuirHinshelwoodReactionRate.H"
+
+namespace Foam
+{
+    makeIRReactions(LangmuirHinshelwoodReactionRate, ThermoPhysics);
+}
+
+
+#include "fluxLimitedLangmuirHinshelwoodReactionRate.H"
+
+namespace Foam
+{
+    makeGeneralReaction
+    (
+        IrreversibleReaction,
+        fluxLimitedLangmuirHinshelwoodReactionRate,
+        ThermoPhysics
+    );
+}
+
+
+// ************************************************************************* //
