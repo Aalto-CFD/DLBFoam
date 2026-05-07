abdbde7169a65f1b9c9f589d9f5089298c1f5139
blttkgl <buluttekgul@gmail.com>
2026-05-07 10:16:00 +0300

    remove chemistry active/inactive print from log

--- src/thermophysicalModels/chemistryModel/chemistryModel/loadBalancedChemistryModel/loadBalancedChemistryModel.C
+++ src/thermophysicalModels/chemistryModel/chemistryModel/loadBalancedChemistryModel/loadBalancedChemistryModel.C
@@ -249,13 +249,9 @@ Foam::scalar Foam::loadBalancedChemistryModel<ThermoType>::solve
                 this->RR(j)[celli] = 0.0;
             }
         }
-        Info << "Chemistry is inactive" << endl;
         return great;
     }
-    else
-    {
-        Info << "Chemistry is active" << endl;
-    }
+
     timer.timeIncrement();
     DynamicList<ChemistryProblem> allProblems = getProblems(deltaT);
     t_getProblems = timer.timeIncrement();
